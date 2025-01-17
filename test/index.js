73234dfbe2872f566361d3f9dbd3b7de3cb603c2
Joël <25136@greijdanus.nl>
2025-01-17 21:23:41 +0100

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,1 +1,10 @@
 //kaas
+//Ik ben iets aan het doen Nielson!!!
+let informatie = document.getElementById("info");
+let standpunten = document.getElementById("standpunt");
+let verandering = document.getElementById("verandering");
+let tekst = document.getElementById("stem")
+
+function veranderTekst() {
+  document.body.style.backgroundColor = "green";
+}
