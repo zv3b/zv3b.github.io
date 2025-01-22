61c99f793e7de0c441f59dc81585bcb4a4bef81f
Joël <25136@greijdanus.nl>
2025-01-22 20:55:42 +0100

    Update index.js

--- test/index.js
+++ test/index.js
@@ -2,18 +2,18 @@
 //Ik ben iets aan het doen Nielson!!!
 let informatie = document.getElementById("info");
 let standpunten = document.getElementById("standpunt");
-let verandering = document.getElementById("verandering");
+let verandering = document.getElementById("change");
 let overons = document.getElementById("overons");
 let tekst = document.getElementById("stem")
 
 function veranderTekst() {
   if (buttonId === "info") {
-    window.location.href = "zv3b.github.io/standpunten.html"; 
+    window.location.href = "https://zv3b.github.io/standpunten.html"; 
   } else if (buttonId === "standpunt") {
-    window.location.href = "zv3b.github.io/standpunten.html"; 
+    window.location.href = "https://zv3b.github.io/standpunten.html"; 
   } else if (buttonId === "verandering") {
-    window.location.href = "zv3b.github.io/standpunten.html"; 
+    window.location.href = "https://zv3b.github.io/standpunten.html"; 
   } else if (buttonId === "overons") {
-    window.location.href = "zv3b.github.io/standpunten.html";
+    window.location.href = "https://zv3b.github.io/standpunten.html";
   } 
 }
