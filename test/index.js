788660c13f9944e6079bc698ebed034727d3b602
Joël <25136@greijdanus.nl>
2025-01-22 20:48:55 +0100

    Update index.js

--- test/index.js
+++ test/index.js
@@ -3,8 +3,19 @@
 let informatie = document.getElementById("info");
 let standpunten = document.getElementById("standpunt");
 let verandering = document.getElementById("verandering");
+let overons = document.getElementById("overons");
 let tekst = document.getElementById("stem")
 
 function veranderTekst() {
-  document.body.style.backgroundColor = "green";
+  if (buttonId === "info") {
+    window.location.href = "zv3b.github.io/standpunten.html"; 
+  } else if (buttonId === "standpunt") {
+    window.location.href = "zv3b.github.io/standpunten.html"; /
+  } else if (buttonId === "verandering") {
+    window.location.href = "zv3b.github.io/standpunten.html"; 
+  } else if (buttonId === "overons") {
+    window.location.href = "zv3b.github.io/standpunten.html";
+  } else {
+    document.body.style.backgroundColor = "green"; 
+  }
 }
