044e61ae0b619cbb313ea7f38180a09cb33c4b0d
SupergaveCheese <25136@greijdanus.nl>
2026-06-19 20:50:41 +0200

    Add startpage.js to load configuration texts

---
+++ startpage.js
@@ -0,0 +1,15 @@
+// index.html texts
+let stemwijzerTitel = document.getElementById("stemwijzerNaam");
+let stemwijzerBeschrijving = document.getElementById("stemwijzerBeschrijving");
+
+window.onload = async function() {
+  await getConfig();
+}
+
+async function getConfig() {
+    const response = await fetch(jsonPath);
+    const config = await response.json();
+    
+    stemwijzerTitel.textContent = config.stemwijzernaam;
+    stemwijzerBeschrijving.textContent = config.stemwijzerbeschrijving;
+}
